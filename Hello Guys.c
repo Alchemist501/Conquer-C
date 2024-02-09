cd7abb4a01753a03a3a6f0606dcfeb5f49290fec
siya15909 <120238075+siya15909@users.noreply.github.com>
2024-02-09 13:22:09 +0530

    Create Hello Guys.c

---
+++ Hello Guys.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+  printf("Hello Everyone!!!\nThis repo contains some codes to help you all to have a basic foundation in C and some advanced topics from DSA");
+  return 0;
+}
